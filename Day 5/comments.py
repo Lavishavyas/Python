70b17cb619db5815ba3c0f43950f1704009a754e
Lavisha Vyas <165463124+Lavishavyas@users.noreply.github.com>
2025-10-11 17:39:22 +0530

    Create comments.py

---
+++ Day 5/comments.py
@@ -0,0 +1,7 @@
+'''Hey Harry , please dont remove this line
+Author : Harry
+Course : 100DaysOfCode'''
+print("Hey I am a good boy \n and this viewer is also a good boy/girl")
+
+# -> Single line comment
+'''-> multi line comment''' #and # this also 
