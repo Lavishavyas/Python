6d39b23ae58d2a69a5e807b02a581aae6ce76929
Lavisha Vyas <165463124+Lavishavyas@users.noreply.github.com>
2025-10-13 00:28:36 +0530

    Create variablesanddatatypes.py

---
+++ Day 6/variablesanddatatypes.py
@@ -0,0 +1,24 @@
+a = 12345678910
+print(a)
+b = "Harry"
+print(b)
+c = True
+d = None
+print("The type of a is : ", type(a))
+print("The type of b is : ", type(b))
+print("The type of c is : ", type(c))
+print("The type of d is : ", type(d))
+
+
+list1 = [[8,2.3], [-4,5], ["apple", "banana"]]
+print(list1)
+# list are mutable
+
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)
+# tuples are immutable
+
+dict1 = {"name" : "Sakshi", "age" : 20, "canVote" : True}
+print(dict1)
+
+# Python me sab kuch object hota hai
