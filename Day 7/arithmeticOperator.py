f0410cec3eb4c78c84158c78c120f7821bdedc3d
Lavisha Vyas <165463124+Lavishavyas@users.noreply.github.com>
2025-10-16 23:38:40 +0530

    Add basic arithmetic operations in arithmeticOperator.py

---
+++ Day 7/arithmeticOperator.py
@@ -0,0 +1,7 @@
+print(5+6)
+print(15-6)
+print(15*6)
+print(15/6)
+print(15//6) #point k baad wale digits hata deta hai floor divison
+print(5%3) #remainder deta hai only
+print(5**3) #exponential (5 to the power 3)
