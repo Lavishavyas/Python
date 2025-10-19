48c6788d16afa6c708b4646f1e179d00710f6b66
Lavisha Vyas <165463124+Lavishavyas@users.noreply.github.com>
2025-10-20 01:13:54 +0530

    Add basic calculator functionality in Python

---
+++ Day 8/calculator(exercise).py
@@ -0,0 +1,12 @@
+#exercise done by me
+
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
+
+print("The addition of a and b is : ", a+b)
+print("The subtraction of a and b is : ", a-b)
+print("The multiplication of a and b is : ", a*b)
+print("The division of a and b is : ", a/b)
+print("The floor division of a and b is : ", a//b)
+print("The remainder of a and b is : ", a%b)
+print("The exponential of a and b is : ", a**b)
