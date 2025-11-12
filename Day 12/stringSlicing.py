0c709e7850b49336152840609f13ec215b244b39
Lavisha Vyas <165463124+Lavishavyas@users.noreply.github.com>
2025-11-12 16:40:02 +0530

    Implement string slicing examples in stringSlicing.py
    Added string slicing examples and print statements.

---
+++ Day 12/stringSlicing.py
@@ -0,0 +1,20 @@
+names = "Harry, Shubham"
+print(names[0:5])
+print(len(names))
+
+fruit = "Mango"
+# len1 = (len(fruit))
+# print("Mango is a", len1, "letter word.")
+mangoLen = len(fruit)
+print(mangoLen)
+# print(fruit[0:4]) #including 0 but not 4
+# print(fruit[1:4]) #including 1 but not 4
+# print(fruit[:5])
+# print(fruit[0:-3])
+# print(fruit[:len(fruit)-3])
+print(fruit[-1:len(fruit)-3])
+print(fruit[-3:-1])
+
+#Quick Quiz
+nm = "Harry"
+print(nm[-4:-2])
